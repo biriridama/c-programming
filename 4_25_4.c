76299f8ac8125f4783432a80d17eb3d495131846
tomohiro <bbp-510-rwm@e-net.nara.jp>
2025-04-25 06:22:01 +0000

    4_25

---
+++ 4_25_4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int num;
+    scanf("%d",&num);
+    if (num%2 !=0)
+    printf("奇数\n");
+    else
+    printf("偶数\n");
+    return 0;
+}
---
+++ 4_25_5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int score;
+    scanf("%d",&score);
+    if (80<=score)
+    printf("合格\n");
+    else
+    printf("不合格\n");
+    return 0;
+}
